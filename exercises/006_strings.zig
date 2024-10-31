318b54a939f301dd760ece9ece71bd309fd8e6e0
Patrick Busenius <patrick.busenius@gmai.com>
2024-10-31 09:21:54 +0100

    solved 006

--- exercises/006_strings.zig
+++ exercises/006_strings.zig
@@ -24,16 +24,16 @@ pub fn main() void {
     // (Problem 1)
     // Use array square bracket syntax to get the letter 'd' from
     // the string "stardust" above.
-    const d: u8 = ziggy['d'];
+    const d: u8 = ziggy[4];
 
     // (Problem 2)
     // Use the array repeat '**' operator to make "ha ha ha ".
-    const laugh = [_]u8{"ha "} ** 3;
+    const laugh = [_]u8{ 'h', 'a', ' ' } ** 3;
 
     // (Problem 3)
     // Use the array concatenation '++' operator to make "Major Tom".
     // (You'll need to add a space as well!)
-    const major = "Major";
+    const major = "Major ";
     const tom = "Tom";
     const major_tom = major ++ tom;
 
