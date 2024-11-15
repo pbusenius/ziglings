a600e1b2d3c08b05485014aed0c56ef7c022a52a
pbusenius <patrick.busenius@gmail.com>
2024-11-15 12:41:26 +0100

    solved 062

--- exercises/062_loop_expressions.zig
+++ exercises/062_loop_expressions.zig
@@ -45,7 +45,9 @@ pub fn main() void {
 
     // Let's find the first language with a three-letter name and
     // return it from the for loop.
-    const current_lang: ?[]const u8 = for (langs) |lang| if (lang.len == 3) break lang else null;
+    const current_lang: ?[]const u8 = for (langs) |lang| {
+        if (lang.len == 3) break lang;
+    } else null;
 
     if (current_lang) |cl| {
         print("Current language: {s}\n", .{cl});
